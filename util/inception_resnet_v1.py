ab495edb307719381355bff8ff1eb9d109581ecd
BingZhang Hu <bingzhanghu1992@gmail.com>
2017-08-21 18:06:33 +0100

    restore from a pre-trained model

--- util/inception_resnet_v1.py
+++ util/inception_resnet_v1.py
@@ -244,3 +244,15 @@ def inception_resnet_v1(inputs, is_training=True,
                         scope='Bottleneck', reuse=False)
   
     return net, end_points
+
+def variable_summaries(var,name):
+    """Attach a lot of summaries to a Tensor (for TensorBoard visualization)."""
+    with tf.name_scope(name+'/summaries'):
+        mean = tf.reduce_mean(var)
+        tf.summary.scalar('mean', mean)
+        with tf.name_scope('stddev'):
+            stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
+        tf.summary.scalar('stddev', stddev)
+        tf.summary.scalar('max', tf.reduce_max(var))
+        tf.summary.scalar('min', tf.reduce_min(var))
+        tf.summary.histogram('histogram', var)
