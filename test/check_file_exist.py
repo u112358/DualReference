46e43454e7e56227f573fd878f562900dc38af9d
BingZhang Hu <bingzhanghu1992@gmail.com>
2017-08-21 21:34:36 +0100

    Added a script to check file exist or not

--- test/check_file_exist.py
+++ test/check_file_exist.py
@@ -5,11 +5,10 @@ CACD = FileReader(data_dir='/scratch/BingZhang/dataset/CACD2000_Cropped',
                   data_info='/scratch/BingZhang/dataset/CACD2000/celenew.mat', reproducible=True,
                   contain_val=False, val_list='./data/val_list.txt')
 path = CACD.path
-for i in path:
+for i in range(CACD.total_images):
     try:
-        path_t = os.path.join(CACD.prefix, i.encode('utf-8'))
+        path_t = os.path.join(CACD.prefix, path[i].encode('utf-8'))
     except:
-        path_t = os.path.join(CACD.prefix, i)
+        path_t = os.path.join(CACD.prefix, path[i])
     if not os.path.isfile(path_t):
-        print(i)
-        
+        print(path[i])
